5b1d8db592363a62ee581f27ac80122bac01a144
Pedro Leandro Batista Marques <pedrolerybr@gmail.com>
2021-06-23 00:17:09 -0300

    add q1

---
+++ questao01/questao01.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 3, j = 5;
+    int *p, *q; //ponteiros de inteiro p e q
+
+    p = &i; //p recebe o endereço da variável i
+    q = &j; //q recebe o endereço da variável j
+
+    p == &i; //verdeiro
+    *p - *q; // -2
+    **&p; //3
+    3 - *p / (*q) + 7; //10
+
+    /*------------------JUSTIFICATIVAS ABAIXO------------------*/
+
+    if (p == &i){
+        printf("p == &i\n"); //p realmente contém o endereço da variável i, então de fato, p=&i
+    }else{
+        printf("p != &i\n"); //isso não é impresso, afinal p == &i
+    }
+
+    printf("*p - *q = %d\n", (*p - *q)); //imprime -2 (CONTEÚDO de p = 3, CONTEÚDO de q = 5, logo: *p - *q = 3 - 5 = -2)
+
+    printf("**&p = %d\n ", **&p); /*
+    imprime 3 (CONTEÚDO do CONTEÚDO do endereço de p)
+    o endereço de p contém o endereço da variável i, e o endereço da variável 3 armazena o valor 3
+    o conteúdo do endereço de p é 3
+    logo, o conteúdo de conteúdo do endereço de p é 3
+    */
+
+    printf("3 - *p/(*q) + 7 = %d\n  ", 3 - *p / (*q) + 7);
+}
